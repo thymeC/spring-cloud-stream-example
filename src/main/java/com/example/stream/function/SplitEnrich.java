7faf7825937dcea07a36fcef5b85bb7d8209b177
LtttAZ <louis_zhang_1303@126.com>
2023-09-10 00:06:01 +0800

    feat: wip

--- src/main/java/com/example/stream/function/SplitEnrich.java
+++ src/main/java/com/example/stream/function/SplitEnrich.java
@@ -3,31 +3,44 @@ package com.example.stream.function;
 import com.example.stream.FakeRepository;
 import com.example.stream.model.EnrichStart;
 import com.example.stream.model.PullRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.stream.function.StreamBridge;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 @Component
 @RequiredArgsConstructor
-public class SplitEnrich implements Consumer<PullRequest> {
+public class SplitEnrich implements Function<KStream<String, PullRequest>, KStream<String, EnrichStart>> {
 
-    private final StreamBridge bridge;
     private final FakeRepository repository;
     private final List<String> enrichments = List.of("JIRA", "BUILD", "SONAR");
 
 
-    @Override
-    public void accept(PullRequest pullRequest) {
-        repository.findPullRequest(pullRequest);
-        for (var enrichment : enrichments) {
-            var ro = new EnrichStart();
-            ro.setType(enrichment);
-            ro.setId(pullRequest.getId());
-            ro.generateKey();
-            bridge.send(FetchEnrich.BINDING_NAME, ro);
-        }
+    private List<EnrichStart> convert(PullRequest value) {
+        return enrichments.stream().map(
+                enrichments -> {
+                    var start = new EnrichStart();
+                    start.setType(enrichments);
+                    start.setId(value.getId());
+                    start.generateKey();
+                    return start;
+                }
+        ).toList();
     }
+
+    @Override
+    public KStream<String, EnrichStart> apply(KStream<String, PullRequest> input) {
+        return input.flatMap((key, value) -> {
+            repository.findPullRequest(value);
+            return convert(value)
+                    .stream()
+                    .map(enrichStart -> new KeyValue<>(enrichStart.getKey(), enrichStart))
+                    .toList();
+        });
+    }
+
 }
